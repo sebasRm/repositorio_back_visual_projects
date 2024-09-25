13c0c9937484be55c8a2ef1030a8e4b9edda7181
sebasRm <71810357+sebasRm@users.noreply.github.com>
2024-09-24 22:14:51 -0500

    Update conection.ts

--- visual_projects_DB/src/db/conection.ts
+++ visual_projects_DB/src/db/conection.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 let sequelize: any;
 
-sequelize = new Sequelize("visual-projects-empresa", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
+sequelize = new Sequelize("visual-projects-db", "root", "rBEyWEYvYtqzwGywgBkWcLciodZwjGlh", {
+    host: "junction.proxy.rlwy.net:24276",
+    dialect: "mysql",
 });
 
 export { sequelize };

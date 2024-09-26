c5f23d1c68a4f2a76db2d3aa4470888f3b9e42e0
sebasRm <71810357+sebasRm@users.noreply.github.com>
2024-09-25 20:06:19 -0500

    Update conection.js

--- visual_projects_DB/dist/src/db/conection.js
+++ visual_projects_DB/dist/src/db/conection.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
 let sequelize;
-exports.sequelize = sequelize = new sequelize_1.Sequelize("visual-projects-db", "root", "", {
-    host: "localhost",
+exports.sequelize = sequelize = new sequelize_1.Sequelize("visual-projects-db", "root", "rBEyWEYvYtqzwGywgBkWcLciodZwjGlh", {
+    host: "mysql.railway.internal",
     dialect: "mysql",
 });
